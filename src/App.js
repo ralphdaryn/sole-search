bfd791348c1ecca0ce65a21aa6f98dea61e47f1c
Ralph <ralphdarync@gmail.com>
2023-08-09 20:38:20 -0400

    update feature branch

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.scss";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/Header/Header";
+import ProductDetails from "./components/ProductDetails/ProductDetails";
+import Home from "./pages/Home/Home";
+import "./utils/api-utils";
 
 function App() {
+  const [sneakers, setSneakers] = useState(null);
+
+  useEffect(() => {
+    axios
+      .get("https://the-sneaker-database.p.rapidapi.com", {
+        headers: {
+          "X-RapidAPI-Key":
+            "9204031a01msh80976ddaee94c44p1d4e54jsn46dbc16662c4",
+        },
+      })
+      .then((response) => {
+        setSneakers(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home sneakers={sneakers} />} />
+        <Route path="/product/:id" element={ProductDetails} />
+        <Route path="*" element={<h1>Page Not Available</h1>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/utils/api-utils.js
@@ -0,0 +1,3 @@
+export const API_URL = "https://the-sneaker-database.p.rapidapi.com";
+export const API_KEY = "9204031a01msh80976ddaee94c44p1d4e54jsn46dbc16662c4";
+export const API_HOST = "the-sneaker-database.p.rapidapi.com";
